00c6290416631ab3baf8e22631e807c680577f05
Manuel <manuel.de.leon@accenture.com>
2021-12-04 19:34:19 -0700

    Add Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
