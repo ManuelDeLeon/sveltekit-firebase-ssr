6f4448ab0f576d154d7818f4c9cfd581244e3c6e
Manuel <manuel.de.leon@accenture.com>
2021-12-04 22:01:32 -0700

    testing

--- src/lib/server/constants.ts
+++ src/lib/server/constants.ts
@@ -5,6 +5,16 @@ if (browser) {
 	throw Error('Cannot load server constants on the client');
 }
 
-export const FIREBASE_PRIVATE_KEY = import.meta.env.VITE_FIREBASE_PRIVATE_KEY.toString();
-export const FIREBASE_CLIENT_EMAIL = import.meta.env.VITE_FIREBASE_CLIENT_EMAIL.toString();
-export const FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID.toString();
+export let FIREBASE_PRIVATE_KEY = '';
+export let FIREBASE_CLIENT_EMAIL = '';
+export let FIREBASE_PROJECT_ID = '';
+
+if (process && process.env.VITE_FIREBASE_PRIVATE_KEY) {
+	FIREBASE_PRIVATE_KEY = process.env.VITE_FIREBASE_PRIVATE_KEY;
+	FIREBASE_CLIENT_EMAIL = process.env.VITE_FIREBASE_CLIENT_EMAIL;
+	FIREBASE_PROJECT_ID = process.env.VITE_FIREBASE_PROJECT_ID;
+} else {
+	FIREBASE_PRIVATE_KEY = import.meta.env.VITE_FIREBASE_PRIVATE_KEY.toString();
+	FIREBASE_CLIENT_EMAIL = import.meta.env.VITE_FIREBASE_CLIENT_EMAIL.toString();
+	FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID.toString();
+}
