6da349c221db9c4bf818279c8024f51e6ff7a89f
Manuel <manueldeleon@gmail.com>
2022-04-01 10:01:21 -0600

    Update dependencies

--- src/routes/api/token.ts
+++ src/routes/api/token.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import cookie from 'cookie';
-export const post: RequestHandler<string> = async (event) => {
+export const post: RequestHandler = async (event) => {
 	const payload = await event.request.json();
 	const token: string = payload.token || '';
 	return {
